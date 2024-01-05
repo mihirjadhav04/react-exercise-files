a14f3c33096b8dca816f0ffd4011c9ae470f8da2
Mihir Jadhav <jadhavmihir143@gmail.com>
2024-01-05 21:04:35 +0530

    studied two ways to create react app and understood the flow and file structure.

---
+++ 01vitereactapp/src/App.jsx
@@ -0,0 +1,22 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+import Test from "./test.jsx"
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    // You can only return one element ithough jsx so <></>.
+    <>
+      <h1> Hello, Mihir from Vite!</h1>
+      <Test/>
+
+    </>
+  
+
+     
+  )
+}
+
+export default App
---
+++ 01vitereactapp/src/test.jsx
@@ -0,0 +1,8 @@
+// Sample test component and make sure your component name should me Capitalcase
+function Test(){
+    return (
+        <p>This is just a test paragraph.</p>
+    )
+}
+
+export default Test
