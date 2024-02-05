61ace4c730c461d7754ff1f9be0105aebf804ac9
Mihir Jadhav <jadhavmihir143@gmail.com>
2024-02-05 16:16:58 +0530

    created new react counter app to learn about hooks in react and how it works.

---
+++ 02counterapp/src/App.jsx
@@ -0,0 +1,39 @@
+import { useState, useCallback } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  // varialbe and function that controls that variable.
+  const [counter, setCounter] = useState(0, )
+  
+  // let counter = 5;
+
+  const addValue = () => {
+    console.log("clicked",counter);
+    let newcounter = counter + 1
+    setCounter(newcounter) // setCounter takes the new value that needs to be added in the counter vairable. 
+    //naming is not compulsory : setCounter / can be keept mihirCounter, etc.
+  }
+
+  const removeValue = () => {
+    setCounter(counter - 1)
+  }
+
+
+  return (
+    <>
+      <h1>React Hooks!</h1>
+      <h2>Counter Value: {counter}</h2>
+      <button
+        onClick={addValue}
+      >Add Value {counter}</button>
+      <button
+        onClick={removeValue}
+      >Remove Value {counter}</button>
+    </>
+  )
+}
+
+export default App
