6b2c1fa7dccaac6bf4826ab1b52ee117c5d0730c
Mihir Jadhav <jadhavmihir143@gmail.com>
2024-02-07 16:02:54 +0530

    project-password-generator completed with learning 2-3 hooks.

--- project-password-generator/src/App.jsx
+++ project-password-generator/src/App.jsx
@@ -1,36 +1,99 @@
-import { useState, useCallback } from 'react'
-import './App.css'
+import { useState, useCallback, useEffect, useRef } from 'react'
+
+
 
 function App() {
+  const [length, setLength] = useState(8)
+  const [numberAllowed, setNumberAllowed] = useState(false);
+  const [charAllowed, setCharAllowed] = useState(false)
+  const [password, setPassword] = useState("") // for memoisation and cahing we have used this
 
-  // Variables 
-  const [ passwordLength, setPasswordLength ] = useState(8)
-  const [ numbersAllowed, setNumbersAllowed ] = useState(false)
-  const [ charactersAllowed, setCharactesAllowed ] = useState(false)
-  const [ password, setPassword ] = useState('')
+  //useRef hook
+  const passwordRef = useRef(null)
 
-  
-  const generatePassword = useCallback(() => {
-    const charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    let additionalCharacters = '';
-    if (numbersAllowed) additionalCharacters += '0123456789';
-    if (charactersAllowed) additionalCharacters += '!@#$%^&*()-_=+';
+  const passwordGenerator = useCallback(() => {
+    let pass = ""
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    if (numberAllowed) str += "0123456789"
+    if (charAllowed) str += "!@#$%^&*-_+=[]{}~`"
 
-    let newPassword = '';
-    const combinedCharset = charset + additionalCharacters;
-
-    for (let i = 0; i < passwordLength; i++) {
-      const randomIndex = Math.floor(Math.random() * combinedCharset.length);
-      newPassword += combinedCharset[randomIndex];
-    }
-    setPassword(newPassword);
-  }, [passwordLength, numbersAllowed, charactersAllowed]);
-
-
-  return (
-    <>
+    for (let i = 1; i <= length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1)
+      pass += str.charAt(char)
       
-    </>
+    }
+
+    setPassword(pass)
+
+
+  }, [length, numberAllowed, charAllowed, setPassword])
+
+  const copyPasswordToClipboard = useCallback(() => {
+    passwordRef.current?.select();
+    passwordRef.current?.setSelectionRange(0, 101);
+    //above 2 lines are written for optimizationa n highlight.
+    window.navigator.clipboard.writeText(password) // here we are able to use window object directly as we are working on core react but in Next.js which runs on server, their we don't have access to this window object.
+  }, [password])
+
+  useEffect(() => {
+    passwordGenerator()
+  }, [length, numberAllowed, charAllowed, passwordGenerator])
+  return (
+    
+    <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500">
+      <h1 className='text-white text-center my-3'>Password generator</h1>
+    <div className="flex shadow rounded-lg overflow-hidden mb-4">
+        <input
+            type="text"
+            value={password}
+            className="outline-none w-full py-1 px-3"
+            placeholder="Password"
+            readOnly
+            ref={passwordRef}
+        />
+        <button
+        onClick={copyPasswordToClipboard}
+        className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'
+        >copy</button>
+        
+    </div>
+    <div className='flex text-sm gap-x-2'>
+      <div className='flex items-center gap-x-1'>
+        <input 
+        type="range"
+        min={6}
+        max={100}
+        value={length}
+         className='cursor-pointer'
+         onChange={(e) => {setLength(e.target.value)}}
+          />
+          <label>Length: {length}</label>
+      </div>
+      <div className="flex items-center gap-x-1">
+      <input
+          type="checkbox"
+          defaultChecked={numberAllowed}
+          id="numberInput"
+          onChange={() => {
+              setNumberAllowed((prev) => !prev);
+          }}
+      />
+      <label htmlFor="numberInput">Numbers</label>
+      </div>
+      <div className="flex items-center gap-x-1">
+          <input
+              type="checkbox"
+              defaultChecked={charAllowed}
+              id="characterInput"
+              onChange={() => {
+                  setCharAllowed((prev) => !prev )
+              }}
+          />
+          <label htmlFor="characterInput">Characters</label>
+      </div>
+    </div>
+</div>
+    
   )
 }
 
