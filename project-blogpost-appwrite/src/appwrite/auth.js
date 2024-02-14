3141b8b6289529c3b46c667d0c0fa4847591ded0
Mihir Jadhav <jadhavmihir143@gmail.com>
2024-02-15 01:18:16 +0530

    generalise auth service written for the blogpost app based on appwrite.

---
+++ project-blogpost-appwrite/src/appwrite/auth.js
@@ -0,0 +1,67 @@
+import config from "../config/config";
+import { Client, Account, ID } from "appwrite";
+
+//service 
+
+export class AuthService {
+    client = new Client();
+    account;
+
+    constructor() {
+        this.client
+            .setEndpoint(config.appWriteUrl)
+            .setProject(config.appWriteProjectId);
+        
+        this.account = new Account(this.client);
+    }
+
+    async createAccount({email, password, name}) {
+        try {
+           const userAccount = await this.account.create(ID.unique,email,password,name)
+           if (userAccount) {
+                // call another method to log him in;
+                return this.login({email, password})
+           }    
+           else{
+                return userAccount 
+           }
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async login( {email, password}) {
+        try {
+            await this.account.createEmailSession(email,password)
+        } catch (error) {
+            throw error
+        }
+    }
+
+    async getCurrentUser(){
+        try {
+            return await this.account.get();
+        } catch (error) {
+            console.log("Appwrite service error:: getCurrentUser :: ", error);
+        }
+
+        return null
+    }
+
+    async logout(){
+        try {
+            await this.account.deleteSessions()
+        } catch (error) {
+            console.log("Appwrite service error:: getCurrentUser :: ", error);
+        }
+    }
+}
+
+
+
+
+
+
+
+const authService = new AuthService() // object
+export default authService // export object directly
